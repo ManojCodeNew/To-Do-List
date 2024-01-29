0383f96755191aae3f32daa8238803a90b69af1e
manoj <manojpoojary2004@gmail.com>
2024-01-29 23:18:12 +0530

    processing

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -5,8 +5,8 @@ export default function navbar() {
         <>
             <div className="bg-blue-900  rounded-md ">
                 <NavLink to='/' className="p-3 text-white text-2xl font-bold text-left pt-5 ">TO DO LIST
-                    <button className="text-yellow-300 text-sm cursor-none">NEW</button></NavLink>
-                    <div className="text-right text-white pb-4">
+                <button className="text-yellow-300 text-sm cursor-none">NEW</button></NavLink>
+                <div className="text-right text-white pb-4">
                 <NavLink to='/' className="pr-10 ">Home</NavLink>
                 <NavLink to='/history' className="m-2 pr-4">History</NavLink>
             </div>
