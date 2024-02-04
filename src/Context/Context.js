80b38975daf15195b9160562c3f96c8a57c24a96
manoj <manojpoojary2004@gmail.com>
2024-02-04 21:20:07 +0530

    context api complete

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -5,7 +5,8 @@ const Global_data = createContext();
 export default function Context({ children }) {
     const [history,setHistory]=useState("Empty")
     const data=[history]
-    const [clicked_item,set_Clicked_Item]=useState();
+    const initial_clicked_item=data.length
+    const [clicked_item,set_Clicked_Item]=useState(initial_clicked_item);
     return <Global_data.Provider value={{data,setHistory,set_Clicked_Item,clicked_item}}>
         {children}
     </Global_data.Provider>
