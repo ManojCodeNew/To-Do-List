9296359f348ea08fe4311954e8202db0658b13bc
manoj <manojpoojary2004@gmail.com>
2024-02-06 12:19:34 +0530

    cilcking

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -6,7 +6,8 @@ export default function Context({ children }) {
     const [history,setHistory]=useState("Empty")
     const data=[history]
     const initial_clicked_item=data.length
-    const [clicked_item,set_Clicked_Item]=useState(initial_clicked_item);
+    const [clicked_item,set_Clicked_Item]=useState(null);
+console.log("history clicked data",clicked_item);
     return <Global_data.Provider value={{data,setHistory,set_Clicked_Item,clicked_item}}>
         {children}
     </Global_data.Provider>
--- src/component/History.js
+++ src/component/History.js
@@ -3,26 +3,37 @@ import Context from "../Context/Context";
 import { Global_data } from "../Context/Context"
 import { useContext } from "react";
 
+import { NavLink } from "react-router-dom";
+
 
 export default function History() {
     // Storing data using useContext
     const todo_history = useContext(Global_data)
     const History_Container=[todo_history.data];
     console.log(History_Container);
+    console.log("Array conformation",Array.isArray(History_Container))
     const Click_handle=(e)=>{
     const Clicked_item_location=e.target.value;
-    
+    const clicked_data=History_Container[0][0][Clicked_item_location]
+    todo_history.set_Clicked_Item(clicked_data)
+   
+    console.log("On Click");
     // return d
     }
     return (
-        <div className="mt-5">
+        <div className="mt-5 ">
+            <h2 className="pl-4">{"Manoj"} To Do History</h2>
             {/* For using one by one display */}
-            {History_Container[0][0].map((item, key) =>
-                <div className="text-3xl border border-solid border-black p-1 mt-5 rounded-lg hover:bg-gray-400 cursor-pointer "  >
-                <button className="hover:font-semibold" onClick={Click_handle} value={key} >{item} </button> 
-                <h1>{key}</h1>
+            {History_Container.flat().flat().map((item, key) =>
+                <div className="text-xl p-1 mt-5 rounded-lg hover:bg-gray-200 hover:border-white cursor-pointer " >
+
+                    {/* Passing */}
+                <NavLink className="hover:font-semibold  " to='/' value={key}>{item} </NavLink> 
                 </div>
             )}
+            {/* <svg class="w-[44px] h-[44px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.3" d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+</svg> */}
         </div>
     )
 }
