0bec6c93c2ec246c700b0c31ae0451858e0dd12b
manoj <manojpoojary2004@gmail.com>
2024-02-07 11:12:53 +0530

    completed build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import NavBar from './component/Navbar1.js';
-import Main from './component/Main.js';
+import Main from './component/Main1.js';
 import History from "./component/History.js";
-// import Profile from './component/Profile.js';
+
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
--- src/component/History.js
+++ src/component/History.js
@@ -8,23 +8,23 @@ import { NavLink } from "react-router-dom";
 export default function History() {
     // Storing data using useContext
     const todo_history = useContext(GlobalData)
-    const History_Container=[todo_history.data];
+    const History_Container = [todo_history.data];
     console.log(History_Container);
     // console.log("Array conformation",Array.isArray(History_Container))
-    const Click_handle=(e)=>{
-    const Clicked_item_location=e.target.value;
-    const clicked_data=History_Container[0][0][Clicked_item_location]
-    todo_history.set_Clicked_Item(clicked_data)
-   
-    // return d
+    const Click_handle = (e) => {
+        const Clicked_item_location = e.target.value;
+        const clicked_data = History_Container[0][0][Clicked_item_location]
+        todo_history.set_Clicked_Item(clicked_data)
+
+        // return d
     }
     let fetchBatteryStatus = async () => {
         let battery = await navigator.getBattery();
         const decimalNumber = battery.level;
-        const percentage = (decimalNumber * 100)+ '%';
-    console.log("On Battery",percentage);
+        const percentage = (decimalNumber * 100) + '%';
+        console.log("On Battery", percentage);
     }
-fetchBatteryStatus();
+    fetchBatteryStatus();
     return (
         <div className="mt-5 ">
             <h2 className="pl-4">{"Manoj"} To Do History</h2>
@@ -33,7 +33,7 @@ fetchBatteryStatus();
                 <div className="text-xl p-1 mt-5 rounded-lg hover:bg-gray-200 hover:border-white cursor-pointer " >
 
                     {/* Passing */}
-                <NavLink className="hover:font-semibold  " to='/' ><button onClick={Click_handle} value={key}>{item} </button></NavLink> 
+                    <NavLink className="hover:font-semibold  " to='/' ><button onClick={Click_handle} value={key}>{item} </button></NavLink>
                 </div>
             )}
             {/* <svg class="w-[44px] h-[44px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
